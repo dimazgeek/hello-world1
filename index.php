2e7e336689acd5955e91a3d765ff53041ef08c8f
dimaz <ardhid@wit.edu>
2016-02-11 10:27:43 +0700

    update

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Even News</title>
+<meta charset="utf-8"/>
+<meta name=viewport content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="css/even.css" />
+</head>
+<body id="body">
+<div id="imagesbg"></div>
+
+
+
+<div class="transbox">
+<h1>profile</h1>
+<div><img src="images/dimaz.jpg" alt="HTML5 Icon" style="width:128px;height:128px"></div>
+<p>name : Dimaz Ardhi,</p>
+<p>email : dimazboston@yahoo.com </p>
+<p>i live in sommervile,MA </p>
+<p>i am currently working as sales marketing,I pursue certificate web technology, I take this class to improve my skill in coding, I know basic web development </p>
+<p>coding skill : php,javascript,html,css</p>
+
+</div>
+
+<div class="transbox1">
+<h1>project 2</h1>
+<p>link:<a href="">project2<a></p>
+<p>git:<a href="">project2<a></p>
+
+</div>
+
+<div class="transbox1">
+<h1>project 3</h1>
+<p>link:<a href="">project3<a></p>
+<p>git:<a href="">project3<a></p>
+
+</div>
+
+<div class="transbox1">
+<h1>project 4</h1>
+<p>link:<a href="">project4<a></p>
+<p>git:<a href="">project4<a></p>
+
+</div>
+
+</body>
+</html>
